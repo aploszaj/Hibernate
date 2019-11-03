b26e2aa9fe5c1c001bd17dbeb84d7c173ed6e37e
Ala <alicja.ploszaj@gmail.com>
2019-11-03 19:21:16 +0100

    implementacja klasy HibernateUtil

--- src/main/java/HibernateUtil.java
+++ src/main/java/HibernateUtil.java
@@ -1,2 +1,29 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateUtil {
+    private static final SessionFactory sessionalFactory;
+
+    static {
+        try {
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml")
+                    .build();
+            Metadata metaData = new MetadataSources(standardRegistry)
+                    .getMetadataBuilder()
+                    .build();
+            sessionalFactory = metaData.getSessionFactoryBuilder().build();
+        }
+        catch (Throwable th){
+            System.err.println("Initial SessionFactory creation failed" + th);
+            throw new ExceptionInInitializerError(th);
+        }
+    }
+
+    public static SessionFactory getSessionalFactory(){
+        return sessionalFactory;
+    }
 }
