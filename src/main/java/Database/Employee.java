4f585d5e606afacb84e57fc94d6d5bc6be6446a6
Ala <alicja.ploszaj@gmail.com>
2019-11-03 21:04:37 +0100

    klasa Employee

---
+++ src/main/java/Database/Employee.java
@@ -0,0 +1,36 @@
+package Database;
+
+import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Employee() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
